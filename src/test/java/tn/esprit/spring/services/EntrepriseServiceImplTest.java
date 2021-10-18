04b1c5c2fc5842f92db75c5c54100ba12de6c620
Chadi-Sassi <chadisassi@gmail.com>
2021-10-18 23:05:25 +0200

    Test Entreprise

---
+++ src/test/java/tn/esprit/spring/services/EntrepriseServiceImplTest.java
@@ -0,0 +1,42 @@
+package tn.esprit.spring.services;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import tn.esprit.spring.entities.Entreprise;
+
+@SpringBootTest
+public class EntrepriseServiceImplTest {
+	@Autowired
+	IEntrepriseService entrepriseservice;
+	private static final Logger l = LogManager.getLogger(EntrepriseServiceImplTest.class);
+	
+	@Test
+	public void testAjouterEntreprise () {
+		Entreprise entreprise=new Entreprise("name","raisonSocial");
+		entrepriseservice.ajouterEntreprise(entreprise);
+		assertThat(entreprise.getId()).isGreaterThan(0);
+	}
+	@Test
+	public void testGetName() {
+		Entreprise entreprise= entrepriseservice.getEntrepriseById(1);
+	//assertThat(entreprise.getName()).isEqualTo("name0");
+	assertNotNull(entreprise.getName());
+	l.info(" entreprise : "+ entreprise.getName());
+	}
+	@Test
+	public void testListDepartement() {
+		List<String> departement = entrepriseservice.getAllDepartementsNamesByEntreprise(1);
+	assertThat(departement).size().isGreaterThan(0);
+	}
+	
+	
+
+}
